8166a93590eff69c1cfd799c0434448baa3bc0f4
SofiaEn9 <sofiaanen9@gmail.com>
2023-06-27 10:51:21 -0600

    Display date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.city;
@@ -11,9 +34,12 @@ function displayTemperature(response) {
   document.querySelector("#wind-speed").innerHTML = Math.round(
     response.data.wind.speed
   );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.time * 1000
+  );
 }
 
 let apiKey = "de926abe0f35af91c8149b6305ofa34t";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Chihuahua&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Paris&key=${apiKey}&units=metric`;
 // console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
