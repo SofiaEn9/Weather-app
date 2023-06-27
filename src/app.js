9888e73dd5651b9265a23409108e7b549c4bfa95
SofiaEn9 <sofiaanen9@gmail.com>
2023-06-27 10:09:07 -0600

    Link to weather API to get current weather info

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#weather-description").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
 
+let apiKey = "de926abe0f35af91c8149b6305ofa34t";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Chihuahua&key=${apiKey}&units=metric`;
+// console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
